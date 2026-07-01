1b79a059e1cad7f8d0d23836b4dc1828a6d29525
pauloricardosilva21-dotcom <paulo.ricardo.silva21@escola.pr.gov.br>
2026-07-01 12:41:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const form = document.getElementById('form')
-const input = document.getElementById('input')
-const todoUl = document.getElementById('todo')
+const form = document.getElementById('form'), input = document.getElementById('input'), todoUl = document.getElementById('todos');
+const save = () => localStorage.setItem('todos', JSON.stringify([...todoUl.children].map(li => ({ text: li.innerText }))));
 
-const todos = JSON.parse(localStorage.getItem('todos'))
+const add = todo => {
+    const li = document.createElement('li');
+    li.innerText = todo.text;
+    li.onclick = () => { li.remove(); save(); };
+    todoUl.appendChild(li);
+};
 
-if(todos){
-    todos.forEach(todo => addTodo(todo))
-}
+const handle = () => {
+    if (input.value.trim()) { add({ text: input.value }); save(); input.value = ''; }
+};
 
+JSON.parse(localStorage.getItem('todos') || '[]').forEach(add);
+form.onsubmit = e => { e.preventDefault(); handle(); };
+window.oncontextmenu = e => { e.preventDefault(); handle(); };
